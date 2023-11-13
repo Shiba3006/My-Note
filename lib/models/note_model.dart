3c849e902c6e9e392a1ddb62bf20ce5879db72c9
Shiba3006 <41759606+Shiba3006@users.noreply.github.com>
2023-11-13 12:28:24 +0200

    create type adabter for note model

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,20 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+// for generated Hive file
+part 'note_model.g.dart';
+
+// flutter packages pub run build_runner build --> terminal code fro generation.
+
+@HiveType(typeId: 0) // hive
+class NoteModel extends HiveObject {
+
+  @HiveType(typeId: 0)
   final String title;
+  @HiveType(typeId: 1)
   final String subTitle;
+  @HiveType(typeId: 2)
   final String date;
+  @HiveType(typeId: 3)
   final int color;
 
   NoteModel(
