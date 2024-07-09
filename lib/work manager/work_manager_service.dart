a6db10cb9044b4eb154fe5a91493306d1aee8a6b
Shehab Ahmed <just.shiba3006@gmail.com>
2024-07-09 22:27:06 +0300

    added work manager sound

---
+++ lib/work manager/work_manager_service.dart
@@ -0,0 +1,39 @@
+import 'dart:developer';
+import 'package:my_note/notification_service/local_notfication_service.dart';
+import 'package:workmanager/workmanager.dart';
+
+
+class WorkManagerService {
+  Future<void> _registerTask() async {
+    await Workmanager().registerPeriodicTask(
+      '1',
+      'simplePeriodicTask',
+      frequency: const Duration(minutes: 15),
+    );
+  }
+
+  Future<void> init() async {
+    await Workmanager().initialize(
+        _actionTask, // The top level function, aka callbackDispatcher
+        isInDebugMode:
+            true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
+        );
+    await _registerTask();
+  }
+
+  Future<void> cancelTask(String uniqueName) async {
+    await Workmanager().cancelByUniqueName(uniqueName);
+  }
+}
+
+@pragma('vm:entry-point')
+void _actionTask() {
+  Workmanager().executeTask((taskName, inputData) async{
+    log('Task $taskName is running! inputData: $inputData');
+    await LocalNotificationService.showDailyNotification(
+      'Daily Notification',
+      'This is an Daily notification',
+    );
+    return Future.value(true);
+  });
+}
